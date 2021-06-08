d6641a810b95fe02a27961c3db4d452d693b9b45
Jason <jasonabeckmann@gmail.com>
2021-06-08 17:07:26 -0400

    Added everything to generate frequency responses and EQ profiles.

--- include/record_frequency_response.h
+++ include/record_frequency_response.h
@@ -1,5 +1,14 @@
 #pragma once
 
 #include <jack/jack.h>
+#include "math.h"
 
 int process (jack_nframes_t nframes, void *arg);
+
+double float_to_db(double value) {
+    return 20*log10(abs(value));
+}
+
+double db_to_float(double db) {
+    return pow(db/20.0, 10.0);
+}
--- src/record_frequency_response.cpp
+++ src/record_frequency_response.cpp
@@ -1,15 +1,37 @@
 #include "record_frequency_response.h"
 
 #include <stdio.h>
+#include <iostream>
 #include <stdlib.h>
 #include <cstring>
+#include <math.h>
+#include <fstream>
 
 #include <jack/jack.h>
 
 jack_port_t *input_port, *output_port;
 jack_client_t *client;
 
+double sin_itr = 0.0;
+#define MIN_FREQ 0
+#define MAX_FREQ 24000.0
+#define FREQ_SPEED 1000.0
+#define FREQ_ITR (FREQ_SPEED/48000.0)
+
+#define DIVISIONS 257
+#define SAMPLES_PER_DIVISION ((double)(MAX_FREQ-MIN_FREQ)/FREQ_SPEED*48000.0/(double)DIVISIONS)
+
+bool done = false;
+bool begin = false;
+
+double RMS[DIVISIONS];
+
+double freq = MIN_FREQ;
+
 int main() {
+	std::ofstream freq_response_file("resp.txt");
+	std::ofstream eq_file("eq.txt");
+
     jack_status_t status;
 
     client = jack_client_open ("Jack Response - Record Freq Res", JackNullOption, &status, NULL);
@@ -41,14 +63,36 @@ int main() {
 		exit (1);
 	}
 
+	memset(RMS, 0, sizeof(RMS));
+
     if (jack_activate (client)) {
 		fprintf (stderr, "cannot activate client");
 		exit (1);
 	}
 
-    while(true) {
+	getchar();
+	begin = true;
+
+    while(!done) {
         
     }
+
+	double full_RMS = 0.0;
+
+	for(int i = 0; i < DIVISIONS; i++) {
+		RMS[i] = sqrt(RMS[i]/SAMPLES_PER_DIVISION);
+		full_RMS += RMS[i]*RMS[i];
+	}
+
+	full_RMS = sqrt(full_RMS/(double)DIVISIONS);
+
+	for(int i = 0; i < DIVISIONS; i++) {
+		std::cout << i << ": " << RMS[i] << "  " << float_to_db(RMS[i]/full_RMS) << "db  " << full_RMS/RMS[i] << "\n";
+		freq_response_file << RMS[i]/full_RMS << "\n";
+		eq_file << full_RMS/RMS[i] << "\n";
+	}
+
+	std::cout << "Full RMS: " << full_RMS << "\n";
 }
 
 int process (jack_nframes_t nframes, void *arg)
@@ -57,7 +101,22 @@ int process (jack_nframes_t nframes, void *arg)
     input = (jack_default_audio_sample_t*)jack_port_get_buffer (input_port, nframes);
 	output = (jack_default_audio_sample_t*)jack_port_get_buffer (output_port, nframes);
 
-	memcpy(output, input, nframes*sizeof(jack_default_audio_sample_t));
+	if(begin) {
+		std::cout << (int)freq << "hz" << std::endl;
+		for(int i = 0; i < nframes; i++) {
+			output[i] = sin(sin_itr);
+			sin_itr+=0.0001309*freq;
+			freq+=FREQ_ITR;
+			if(freq >= MAX_FREQ) {
+				done = true;
+				return 0;
+			}
+
+			int RMS_index = floor((freq-MIN_FREQ) / ((MAX_FREQ-MIN_FREQ)/(double)DIVISIONS));
+
+			RMS[RMS_index] += input[i]*input[i];
+		}
+	}
 
 	return 0;      
 }
